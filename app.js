63a601f7d2ad132aa927d604f3d1b70ee9977557
Yay <wangyegp@gmail.com>
2015-11-09 18:21:44 +0700

    Deletes comments for studying process.

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var cats = {
     'alt': 'the cat Jessie on bookshelf'
   }],
 
-  //  The following object binds individual cat names to a corresponding DOM.
+  /* The following object binds individual cat names to
+   * corresponding DOM, to display when clicked.
+  */
   'catDivs': {},
   'catClicks': {}
 };
@@ -32,7 +34,6 @@ cats.HTMLnavEntry = '<li class="nav-entry">' +
   '<a href="#%lowerCaseName%"><h3>%data%</h3></a></li>';
 
 for (var i = 0; i < cats.cats.length; i++) {
-
   var name = cats.cats[i].name;
   var lowerCaseName = name.toLowerCase();
   var alt = cats.cats[i].alt;
@@ -40,62 +41,27 @@ for (var i = 0; i < cats.cats.length; i++) {
   var HTMLnavEntry = cats.HTMLnavEntry.replace('%data%', name).replace('%lowerCaseName%', lowerCaseName);
   $('.nav').append(HTMLnavEntry);
 
-  /* the 'cat' in cat-img is not replaced. All the cat images should have the same class names */
+  // The 'cat' in cat-img is not replaced. All cat images should have the same class names.
   var HTMLcatDiv = cats.HTMLcatDiv.replace('%catName%', name).replace(/%lowerCaseName%/g, lowerCaseName).replace('%catAlt%', alt);
   cats.catDivs[lowerCaseName] = HTMLcatDiv;
 
-  // initial clicks number is 0
+  // The initial clicks number is 0.
   cats.catClicks[lowerCaseName] = 0;
 }
 
-// TODO: right now the first time a nav-entry is clicked, the content
-// html will work. Then it won't. Let's toggle CSS classes instead
-// TODO: wrap all the code
-
-// IIFE doesn't work inside the click event here.
-// no two bug happened: first there was a # in href attribute
-// making it different from a name
-// second cats.catDivs.name doesn't work since name is a var
 $('.nav-entry').each(function() {
   var name = $(this).text().toLowerCase();
   $(this).click(function() {
-
     $('.content').html(cats.catDivs[name]);
-    // this ensures that the each time the catDiv loads, the count isn't 0
+    // Upom each loading, the count isn't 0
     $('.count-display').text(cats.catClicks[name]);
 
     $('.cat').click(function() {
-      //console.log('clicked');
-      // ensures searching multiple levels by locating .cat and then using find
-      // accomodates adding divs for styling
-      // I made a mistake of using this instead of $(this)
-      // and i tried to find the a tag while there's no links
-
-      // this part was initially outside of .each, where it won't work
       cats.catClicks[name] += 1;
-      // this class name is unqiue, so why not just select
-      // it from the whole file?
       $('.count-display').text(cats.catClicks[name]);
     });
   });
-  // Uses the fact that catId is the same as complete internal
-  // href attritute, starting with #
-  // var catId = $(this).children('a').attr('href');
 });
-
-/*
-$('.cat').click(function() {
-  console.log('clicked');
-  // ensures searching multiple levels by locating .cat and then using find
-  // accomodates adding divs for styling
-  // I made a mistake of using this instead of $(this)
-  var catId = $(this).parents('.cat').find('a').attr('href');
-  var catName = catId.slice(1);
-
-  cats.catClicks[catName] += 1;
-  $(catId).children('.count-display').text(cats.catClicks[catName]);
-});
-*/
 
 $('.nav-button').click(function() {
   $('.nav').toggleClass('toggleDisplay');
